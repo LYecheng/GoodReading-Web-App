b94b66d81cfb7ada9ca68f36b5647422e24fd612
Yecheng_Li <lyc@uchicago.edu>
2015-06-08 21:24:35 -0500

    init

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -6,7 +6,7 @@ class FavoritesController < ApplicationController
     if session[:user_id].blank?
       redirect_to book_url, notice: 'Please log in first.'
     else
-      @user = User.find_by(id: session['user_id']
+      @user = User.find_by(id: session['user_id'])
     end
   end
 
